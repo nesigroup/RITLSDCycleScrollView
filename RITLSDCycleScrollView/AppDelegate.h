2ecc7c0e372e2a4c84605db1b9b4b3cec194ea4f
774655048@qq.com <774655048@qq.com>
2018-01-15 22:10:08 +0800

    Initial Commit

---
+++ RITLSDCycleScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RITLSDCycleScrollView
+//
+//  Created by YueWen on 2018/1/15.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RITLSDCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITLSDCycleScrollView
+//
+//  Created by YueWen on 2018/1/15.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
